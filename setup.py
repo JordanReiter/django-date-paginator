6d9896fb157031b022ca7b0a9abfb025788817d9
Jordan Reiter <jordan@aace.org>
2015-09-11 19:07:47 -0400

    Setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='django-date-paginator',
+      version='1.0',
+      description='Paginate by date',
+      author='Damien Szczyt',
+      author_email='damien.szczyt@gmail.com',
+      packages=['date_paginator'],
+     )
+
